b7b04b239162521bdb9074f7ce478db305531ac2
Cyrhades <cyrhades76@gmail.com>
2020-06-25 10:51:22 +0200

    Correction est amélioration des getBy (ajou de findBy et en One également).

--- src/AbstractRepository.php
+++ src/AbstractRepository.php
@@ -14,8 +14,10 @@
 
     public function __call($method, $params = [])
     {
-        if(strpos($method, 'getBy') !== false) {
-            $this->getByAnythingField($method, $params);
+        if(strpos($method, 'getBy') !== false || strpos($method, 'findBy') !== false) {
+           return $this->findByAnythingField($method, $params);
+        } elseif(strpos($method, 'getOneBy') !== false || strpos($method, 'findOneBy') !== false) {
+           return $this->findByAnythingField($method, $params, true);
         }
     }
 
@@ -23,12 +25,14 @@
      * Cette méthode permet de gérer toute les type de requetes 
      * du type getById, getByEmail, etc 
      */
-    private function getByAnythingField($method, $params = [])
+    private function findByAnythingField($method, $params = [], $onlyOne = false)
     {
-        $property = str_replace('getBy','', $method); 
+        $property = str_replace(['findBy','getBy','findOneBy','getOneBy'],'', $method); 
         $entityName = str_replace(['Repository','\\\\'],['','\\Entity\\'], \get_called_class()); 
+
         // On vérifie que la propriété existe dans l'entity
         if(property_exists($entityName, lcfirst($property))) {
+            
             $field = strtolower($property); 
             $conn = $this->getEntityManager($entityName);
             $table = substr($entityName, strrpos($entityName, '\\')+1).'s';
@@ -36,10 +40,13 @@
             $query = $conn->prepare('SELECT * FROM `'.$table.'` WHERE `'.$field.'`=?');
             $query->setFetchMode(\PDO::FETCH_CLASS, $this->entity);
             $query->execute($params);
-            $entity = $query->fetch() ?? null;
             
-            return $entity;
+            if($onlyOne) {
+                return $query->fetch() ?? null;
+            }
+            else {
+                return $query->fetchAll() ?? null;
+            }
         }
     }
 }
- 
