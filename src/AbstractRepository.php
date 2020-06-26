615eda1a44569827f52fd1a90515dec4f53c8e40
Cyrhades <cyrhades76@gmail.com>
2020-06-26 09:24:26 +0200

    Amélioration de l'AbstractRepository

--- src/AbstractRepository.php
+++ src/AbstractRepository.php
@@ -1,23 +1,30 @@
 <?php
  
- namespace Yoop;
+namespace Yoop;
   
- abstract class AbstractRepository
- {
+abstract class AbstractRepository
+{
     protected $entity; 
 
-    protected function getEntityManager($classe)
+    protected function getEntityManager(?string $classe = null)
     {
-        $this->entity = $classe;
+        if($classe === null) {
+            $this->entity =  str_replace(['Repository','\\\\'],['','\\Entity\\'], \get_called_class()); 
+        } else {
+
+            $this->entity = $classe;
+        }
         return DatabaseMySql::getDb();
     }
 
-    public function __call($method, $params = [])
+    public function __call(string $method, array $params = [])
     {
         if(strpos($method, 'getBy') !== false || strpos($method, 'findBy') !== false) {
            return $this->findByAnythingField($method, $params);
         } elseif(strpos($method, 'getOneBy') !== false || strpos($method, 'findOneBy') !== false) {
            return $this->findByAnythingField($method, $params, true);
+        } elseif(strpos($method, 'get') !== false || strpos($method, 'find') !== false) {
+            return $this->findAnything($method, ...$params);
         }
     }
 
@@ -28,14 +35,13 @@
     private function findByAnythingField($method, $params = [], $onlyOne = false)
     {
         $property = str_replace(['findBy','getBy','findOneBy','getOneBy'],'', $method); 
-        $entityName = str_replace(['Repository','\\\\'],['','\\Entity\\'], \get_called_class()); 
+        $conn = $this->getEntityManager();
 
         // On vérifie que la propriété existe dans l'entity
-        if(property_exists($entityName, lcfirst($property))) {
+        if(property_exists($this->entity, lcfirst($property))) {
             
-            $field = strtolower($property); 
-            $conn = $this->getEntityManager($entityName);
-            $table = substr($entityName, strrpos($entityName, '\\')+1).'s';
+            $field = lcfirst($property); 
+            $table = substr($this->entity, strrpos($this->entity, '\\')+1).'s';
 
             $query = $conn->prepare('SELECT * FROM `'.$table.'` WHERE `'.$field.'`=?');
             $query->setFetchMode(\PDO::FETCH_CLASS, $this->entity);
@@ -48,5 +54,38 @@
                 return $query->fetchAll() ?? null;
             }
         }
+    }
+
+
+    /**
+     * Cette méthode permet de gérer toute les type de requetes find
+     */
+    private function findAnything(string $method, string $where = '', array $params = [], int $limit = 0, int $offset = 0)
+    {
+        $property = str_replace(['find','get'],'', $method); 
+        $conn = $this->getEntityManager();
+
+        $table = substr($this->entity, strrpos($this->entity, '\\')+1).'s';
+        $where = $this->constructWhere($where, $limit, $offset);
+    
+        $query = $conn->prepare('SELECT * FROM `'.$table.'` '.$where);
+        $query->setFetchMode(\PDO::FETCH_CLASS, $this->entity);
+        $query->execute($params);
+     
+        return $query->fetchAll() ?? null;
+    }
+
+
+    private function constructWhere(string $where = '', int $limit = 0, int $offset = 0)
+    {
+        $where = (!empty($where)) ? ' WHERE '.$where : '';
+      
+        if($limit > 0) {
+            $where .= ' LIMIT '.intval($limit);
+        }
+        if($offset > 0) {
+            $where .= ' OFFSET '.intval($offset);
+        }
+        return $where;
     }
 }
