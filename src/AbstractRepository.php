96f97c09f78c85cd93874ca33793d322471d708d
Cyrhades <cyrhades76@gmail.com>
2023-11-23 20:08:11 +0100

    Déplacé la fonction getEntity pour pouvoir y accéder directement depuis les controllers

--- src/AbstractRepository.php
+++ src/AbstractRepository.php
@@ -31,33 +31,4 @@ abstract class AbstractRepository
         else
             throw new Exception('No database Connect');
     }
-
-    /**
-     * Cette méthode me permet de retoruner un tableau 
-     * associatif à partir d'une entity
-     */
-    protected function getEntity(EntityInterface $entity) 
-    {
-        if(is_object($entity)) {   
-            $result = [];
-            // Boucle sur les attribut de l'entiity
-            foreach ((array) $entity as $key => $value)
-            {
-                // exemple de $key : "App\Entity\Customer firstName"
-                // attributName recevra uniquement "firstName" par rapport à notre exemple
-                $attributName = trim(substr($key, strrpos($key, chr(0))));
-                // à partir de attributName on va créer le nom du getter (exemple : getFirstName)
-                $getter = 'get'.ucfirst($attributName); 
-                // si la méthode existe et que la valeur n'est pas null
-                if(method_exists($entity, $getter) && is_null($value) === false) {
-                    // on l'ajoute à notre tableau associatif
-                    $result[$attributName] = call_user_func([$entity,$getter]);
-                }
-            }
-            // retourne le tableau associatif créé à partir de l'entity
-            return $result;
-        }
-        return; // retourne null
-    }
-
 }
